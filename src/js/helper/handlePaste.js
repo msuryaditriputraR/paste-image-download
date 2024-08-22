f00062b621b86f6dfef92aa890d631574f895fcc
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-08-22 10:41:53 +0800

    add: helper handlePaste

---
+++ src/js/helper/handlePaste.js
@@ -0,0 +1,18 @@
+import downloadImage from "./downloadImage.js";
+export default async function handlePaste() {
+    const clipboardItems = await navigator.clipboard.read();
+    if (!clipboardItems) {
+        console.info("There is no clipboard data");
+    }
+    else {
+        for (const clipboardItem of clipboardItems) {
+            //
+            for (const itemType of clipboardItem.types) {
+                if (itemType.includes("image")) {
+                    const parsedItem = await clipboardItem.getType(itemType);
+                    downloadImage(parsedItem);
+                }
+            }
+        }
+    }
+}
